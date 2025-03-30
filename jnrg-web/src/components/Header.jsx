46e9b8312185d8ce5f38ef89adb8ed69b177687b
PeterGithua29 <juniorgithua@gmail.com>
2025-03-30 19:14:54 +0000

    project creation

---
+++ jnrg-web/src/components/Header.jsx
@@ -0,0 +1,7 @@
+export default function Header(){
+    return (
+        <header classname="w-full bg-blue-500 text-white p-4 text-center">
+            <h2 classname="text-lg font-semibold">My Website Header</h2>
+        </header>
+    );
+}
